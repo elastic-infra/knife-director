b916f64e7e737849054e36ca036fff1929233429
Tomoya Kabe <limit.usus@gmail.com>
2017-08-26 18:38:48 +0900

    Init repo

---
+++ lib/knife/director.rb
@@ -0,0 +1,7 @@
+require "knife/director/version"
+
+module Knife
+  module Director
+    # Your code goes here...
+  end
+end
---
+++ lib/knife/director/version.rb
@@ -0,0 +1,5 @@
+module Knife
+  module Director
+    VERSION = "0.1.0"
+  end
+end
